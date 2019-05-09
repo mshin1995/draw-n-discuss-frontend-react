c6133d26c5175473d188017f6c73da31932c6d28
github username <github email address>
2019-05-09 12:15:33 -0700

    first heroku commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = "http://localhost:3000";
-export const API_WS_ROOT = "ws://localhost:3000/cable"; // WS = web-sockets
+export const API_ROOT = "https://frozen-coast-60360.herokuapp.com/";
+export const API_WS_ROOT = "ws://frozen-coast-60360.herokuapp.com/cable"; // WS = web-sockets
 export const HEADERS = {
   'Content-Type': 'application/json',
   'Accept': 'application/json'
